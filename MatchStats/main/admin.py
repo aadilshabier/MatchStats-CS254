dfbee582bbab80019db20ff3883c126161067aab
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-05-02 00:30:20 +0530

    Add Fields in the Match mode and applied migrations

--- MatchStats/main/admin.py
+++ MatchStats/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team, Player, Stadium, Match, Transfer, Goal
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Stadium)
+admin.site.register(Match)
+admin.site.register(Transfer)
+admin.site.register(Goal)
