64dd2e9f5249960f39688d20a4ed9880cab1c082
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-05-01 23:01:07 +0530

    Make changes to models.py

--- MatchStats/MatchStats/settings.py
+++ MatchStats/MatchStats/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user.apps.UserConfig',
     'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- MatchStats/main/models.py
+++ MatchStats/main/models.py
@@ -1,19 +1,25 @@
 from django.db import models
-from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 
+class Stadium(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    address = models.CharField(max_length=512)
+    city = models.CharField(max_length=64)
+    capacity = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
 
+class Team(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    code = models.CharField(max_length=3, unique=True)
+    stadium = models.ForeignKey(Stadium, on_delete=models.SET_NULL, null=True, blank=True)
+    goals_scored = models.PositiveSmallIntegerField()
+    goals_conceded = models.PositiveSmallIntegerField()
+    points = models.SmallIntegerField()
 
-# Create your models here.
-
-class Teams(models.Model):
-    team_id = models.IntegerField(primary_key=True)
-    team_name = models.CharField(max_length=50, null = False)
-    team_code = models.CharField(max_length=3)
-    home_stadium = models.IntegerField()
-    goals_scored = models.IntegerField()
-    goals_conceded = models.IntegerField()
-    points = models.IntegerField()
+    def __str__(self):
+        return self.name
 
 
 class Player(models.Model):
@@ -24,60 +30,49 @@ class Player(models.Model):
         ('F',"Forward"),
     ]
 
-    p_id = models.IntegerField(primary_key = True)
-    p_name = models.CharField(max_length=50)
-    # team = models.CharField(max_length=50)
-    jersey_no = models.PositiveSmallIntegerField()
-    dob = models.DateField( null = False, blank = False)
-    position = models.CharField(choices=POSITION, max_length=1)
-    team_id = models.ForeignKey(Teams, to_field = 'team_id' ,on_delete = models.SET_NULL, null = True)
-    goals = models.IntegerField(default=0, null = True)
-    assists = models.IntegerField(default=0, null = True)
-    yellow_cards = models.IntegerField(default=0, null = True)
-    red_cards = models.IntegerField(default=0, null = True)
+    name = models.CharField(max_length=128)
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete = models.SET_NULL)
+    jersey_no = models.PositiveSmallIntegerField(null=True)
+    dob = models.DateField()
+    position = models.CharField(max_length=1, choices=POSITION)
+    goals = models.PositiveSmallIntegerField(default=0)
+    assists = models.PositiveSmallIntegerField(default=0)
+    yellow_cards = models.PositiveSmallIntegerField(default=0)
+    red_cards = models.PositiveSmallIntegerField(default=0)
 
-class Stadium(models.Model):
-    stadium_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length= 200)
-    city = models.CharField(max_length=50)
-    capacity = models.IntegerField(default = 0)
+    def __str__(self):
+        return self.name
 
-class Matches(models.Model):
-    match_id = models.IntegerField(primary_key=True)
-    team_1 = models.ForeignKey(Teams,related_name='team_1', to_field='team_id', on_delete= models.CASCADE)
-    team_2 = models.ForeignKey(Teams,related_name='team_2', to_field='team_id', on_delete= models.CASCADE)
-    score_1 = models.PositiveSmallIntegerField(default= 0 )
-    score_2 = models.PositiveSmallIntegerField(default = 0)
+class Match(models.Model):
+    team_1 = models.ForeignKey(Team, null=True, related_name="team_1", on_delete=models.SET_NULL)
+    team_2 = models.ForeignKey(Team, null=True, related_name="team_2", on_delete=models.SET_NULL)
+    score_1 = models.PositiveSmallIntegerField(default=0)
+    score_2 = models.PositiveSmallIntegerField(default=0)
     match_time = models.DateTimeField()
     completed = models.BooleanField(default=False)
-    stadium_id = models.ForeignKey(Stadium, to_field='stadium_id', on_delete=models.CASCADE)
-    viewer_count = models.PositiveIntegerField(default=0)
+    stadium = models.ForeignKey(Stadium, null=True, on_delete=models.SET_NULL)
+    viewer_count = models.PositiveIntegerField(null=True, blank=True)
 
-class Transfers(models.Model):
-    transfer_id = models.IntegerField(primary_key=True)
-    team_from_id = models.ForeignKey(Teams, related_name = 'team_from', to_field= 'team_id', on_delete=models.CASCADE)
-    team_to_id = models.ForeignKey(Teams, related_name = 'team_to',to_field='team_id', on_delete=models.CASCADE)
-    player_id = models.ForeignKey(Player, to_field='p_id', on_delete=models.CASCADE)
-    price = models.FloatField()
-    transfer_date = models.DateField( null = False, blank = False)
+class Transfer(models.Model):
+    team_from = models.ForeignKey(Team, null=True, related_name="team_from", blank=True, on_delete=models.SET_NULL)
+    team_to = models.ForeignKey(Team, null=True, related_name="team_to", blank=True, on_delete=models.SET_NULL)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    price = models.FloatField(validators=[MinValueValidator(0.0)])
+    transfer_date = models.DateField()
 
+# class SupportStaff(models.Model):
+#     STAFF_JOB_LIST = [
+#         ('mgr', 'Manager'),
+#         ('ch', 'Coach')
+#     ]
+#     staff_id = models.IntegerField(primary_key=True)
+#     name = models.CharField(max_length=30)
+#     type = models.CharField(choices=STAFF_JOB_LIST, max_length=20)
+#     team_id = models.ForeignKey(Teams, to_field='team_id', on_delete=models.CASCADE)
 
-class SupportStaff(models.Model):
-    STAFF_JOB_LIST = [
-        ('mgr', 'Manager'),
-        ('ch', 'Coach')
-    ]
-    staff_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30)
-    type = models.CharField(choices=STAFF_JOB_LIST, max_length=20)
-    team_id = models.ForeignKey(Teams, to_field='team_id', on_delete=models.CASCADE)
-
-class Goals(models.Model):
-    goal_id = models.IntegerField()
-    match_id = models.ForeignKey(Matches, to_field='match_id', on_delete=models.CASCADE)
-    team_id = models.ForeignKey(Teams, to_field = 'team_id' ,on_delete = models.SET_NULL, null = True)
-    player_id = models.ForeignKey(Player,related_name='player', to_field='p_id', on_delete=models.CASCADE)
-    assist_id = models.ForeignKey(Player, related_name='assist',to_field='p_id', on_delete=models.CASCADE)
+class Goal(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, null=True, on_delete = models.SET_NULL)
+    scorer = models.ForeignKey(Player, null=True, related_name="goal_set", on_delete=models.SET_NULL)
+    assist = models.ForeignKey(Player, null=True, related_name="assist_set",blank=True, on_delete=models.SET_NULL)
     match_time = models.DateTimeField()
-    
