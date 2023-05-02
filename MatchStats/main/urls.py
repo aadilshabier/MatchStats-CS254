051c880e579d63cbc8a1bb24ff20b72bbb74671d
L0STW0RLD <meghanagupta2003@gmail.com>
2023-05-02 15:59:00 +0530

    made views and templates

--- MatchStats/main/urls.py
+++ MatchStats/main/urls.py
@@ -6,4 +6,9 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("teams", views.TeamsList.as_view(), name = "teamList"),
+    path("team/<int:team_id>", views.TeamDetail.as_view(), name="teamDetail"),
+    path("match/<int:match_id>", views.MatchDetail.as_view(), name="matchDetail"),
+    path("matches", views.MatchList.as_view(), name="matchList"),
+    path("stadiums", views.StadiumList.as_view(), name="stadiumList"),
 ]
