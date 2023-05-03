9fc7eb4b80813a5dc808cbd30b05e2edb1610d06
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-05-03 08:20:50 +0530

    Added teams list view(unfinished)

--- MatchStats/main/urls.py
+++ MatchStats/main/urls.py
@@ -6,7 +6,7 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("teams", views.TeamsList.as_view(), name = "teamList"),
+    path("teams", views.TeamsList.as_view(), name = "team_list"),
     path("team/<int:team_id>", views.TeamDetail.as_view(), name="teamDetail"),
     path("match/<int:match_id>", views.MatchDetail.as_view(), name="matchDetail"),
     path("matches", views.MatchList.as_view(), name="matchList"),
--- MatchStats/main/views.py
+++ MatchStats/main/views.py
@@ -1,38 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
-from .models import Stadium, Player, Team, Match, Goal
-
-from django.views.generic import ListView, DetailView
+from .models import Stadium, Player, Team, Match
 
 def index(request):
     return HttpResponse(b"MatchStats app")
 
-class TeamsList(ListView):
+class TeamsList(generic.ListView):
     model = Team
     template_name = 'main/team_list.html'
+    context_object_name = "teams"
+    ordering = ["-points"]
 
-class TeamDetail(DetailView):
+class TeamDetail(generic.DetailView):
     model = Team
     template_name = 'main/team_detail.html'
 
-class StadiumList(ListView):
+class StadiumList(generic.ListView):
     model = Stadium
     template_name = 'main/stadium_list.html'
 
-class MatchList(ListView):
+class MatchList(generic.ListView):
     model = Match
     template_name = 'main/match_list.html'
 
 
-class MatchDetail(DetailView):
+class MatchDetail(generic.DetailView):
     model = Match
     template_name = 'main/match_detail.html'
 
-class PlayerDetail(DetailView):
+class PlayerDetail(generic.DetailView):
     model = Player
     template_name = 'main/player_detail.html'
 
-class PlayersList(ListView):
+class PlayersList(generic.ListView):
     model = Player
     template_name = 'main/player_list.html'
