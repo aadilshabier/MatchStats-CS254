f9822d73cfe3968aa8d83515a40fe99b0a18ce79
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-05-31 17:21:12 +0530

    Added login/logout and changed navbar

--- MatchStats/user/urls.py
+++ MatchStats/user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.shortcuts import redirect
 
 from . import views
 from .apps import UserConfig
@@ -9,5 +8,9 @@ app_name = UserConfig.name
 
 urlpatterns = [
     path("login/",
-        auth_views.LoginView.as_view(template_name="user/login.html"), ),
+        auth_views.LoginView.as_view(template_name="user/login.html", next_page="main:index"),
+        name="login"),
+    path("logout/",
+        auth_views.LogoutView.as_view(next_page="main:index"),
+        name="logout"),
 ]
