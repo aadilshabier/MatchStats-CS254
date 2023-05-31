2669a2db70fd560a4bdf3e13fec88a6eb8f66f18
saikiran0101 <saikirannayak1430@gmail.com>
2023-05-31 12:08:09 +0530

    template changes

--- MatchStats/main/urls.py
+++ MatchStats/main/urls.py
@@ -7,8 +7,10 @@ app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("teams", views.TeamsList.as_view(), name = "team_list"),
-    path("team/<int:team_id>", views.TeamDetail.as_view(), name="teamDetail"),
-    path("match/<int:match_id>", views.MatchDetail.as_view(), name="matchDetail"),
+    path('team/<int:pk>/', views.TeamDetail.as_view(), name="team_detail"),
+    path('<int:pk>/', views.MatchDetail.as_view(), name="matchDetail"),
     path("matches", views.MatchList.as_view(), name="match_list"),
     path("stadiums", views.StadiumList.as_view(), name="stadium_list"),
+    path('player/<int:pk>/', views.PlayerDetail.as_view(), name='player_detail'),
+    path('players',views.PlayersList.as_view(),name="player_list"),
 ]
